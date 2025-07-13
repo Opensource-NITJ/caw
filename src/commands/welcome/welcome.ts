3e7c44ffd49a06b1635db53e6887d69fea17e765
Aumansh Kaushal <aumanshk@gmail.com>
2025-07-13 13:39:57 +0530

    Implement welcome module with channel messaging and remove roles.json

---
+++ src/commands/welcome/welcome.ts
@@ -0,0 +1,23 @@
+import Eris from 'eris';
+import { Command } from '../../types/command';
+import emoji from '../../../secret/emoji.json';
+import fs from 'fs';
+import channel from '../../../secret/channel.json';
+import config from '../../../secret/config.json';
+
+export default (bot: Eris.Client): Command => ({
+    name: 'welcome_module',
+    description: 'Handles the welcome message',
+    type: 'guildMemberAdd',
+    async execute(guild: Eris.Guild, member: Eris.Member): Promise<void> {
+        
+        if (guild.id !== config.guildID) return;
+
+        await bot.createMessage(channel.welcome, {
+            content: [
+                `Howdy <@${member.id}>! Welcome to **${guild.name}**!`,
+                `Please take your roles from <#${channel.roles}> to get access to the server.`,
+            ].join('\n'),
+        })
+    }
+});
